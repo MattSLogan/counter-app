44c1cd34da21f94c72048b727f08ea21d3c79e06
MattSLogan <msl5675@psu.edu>
2025-02-27 17:18:25 -0500

    Latest Modifications

--- counter-app.js
+++ counter-app.js
@@ -20,6 +20,9 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
+    this.min = -15;
+    this.max = 15;    
+    this.count = 0;
     this.title = "";
     this.t = this.t || {};
     this.t = {
@@ -39,7 +42,9 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   static get properties() {
     return {
       ...super.properties,
-      title: { type: String },
+      count: { type: Number, reflect: true },
+      min: { type: Number, reflect: true },
+      max: { type: Number, reflect: true },
     };
   }
 
@@ -47,9 +52,15 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   static get styles() {
     return [super.styles,
     css`
+      :host([count="18"]) {
+        color: var(--ddd-theme-default-athertonViolet);
+      }
+      :host([count="21"]) {
+        color: var(--ddd-theme-default-athertonViolet);
+      }
       :host {
         display: block;
-        color: var(--ddd-theme-primary);
+        color: var(--counter-color, var(--ddd-theme-primary));
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
@@ -60,16 +71,94 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
       h3 span {
         font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
       }
+      .counter {
+        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+      }
+      .buttons button {
+        background-color: var(--ddd-theme-default-skyLight);
+        ;
+      }
+      .buttons button:hover {
+        background-color: (--ddd-theme-default-keystoneYellow);
+      }
+      .buttons button:focus {
+        background-color: var(--ddd-theme-default-keystoneYellow);
+      }
     `];
   }
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+  
+    if (changedProperties.has("count")) {
+      if (this.count === this.max || this.count === this.min) {
+        this.style.setProperty("--counter-color", "var(--ddd-theme-default-athertonViolet)");
+      } else {
+        this.style.setProperty("--counter-color", "var(--ddd-theme-primary)");
+      }
+  
+      if (this.count === 21) {
+        this.makeItRain();
+      }
+    }
+  }
+  
+  
+
+makeItRain() {
+  // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+  // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+  // will only run AFTER the code is imported and available to us
+  import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+    (module) => {
+      // This is a minor timing 'hack'. We know the code library above will import prior to this running
+      // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+      // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+      // it's listening for changes so it can react
+      setTimeout(() => {
+        // forcibly set the poppped attribute on something with id confetti
+        // while I've said in general NOT to do this, the confetti container element will reset this
+        // after the animation runs so it's a simple way to generate the effect over and over again
+        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+      }, 0);
+    }
+  );
+}
+
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+<confetti-container id="confetti" class="wrapper">
+<div class="counter">${this.count}</div>
+<div class="buttons">
+    <button @click="${this.decrease}" ?disabled="${this.min === this.count}">-1</button>
+    <button @click="${this.reset}">Reset</button>
+    <button @click="${this.increase}" ?disabled="${this.max === this.count}">+1</button>
+    </div>
+  </div>
+</confetti-container>
+`;
+}
+
+  increase() {
+    if (this.count < this.max) {
+      this.count++;
+    }
+  }
+  decrease() {
+    if (this.count > this.min) {
+    this.count--;
+    }
+  }
+  reset() {
+    this.count = this.__counter;
+  }
+
+  firstUpdated(changedProperties) {
+    super.updated(changedProperties);
+    this.__counter = this.count;
   }
 
   /**
